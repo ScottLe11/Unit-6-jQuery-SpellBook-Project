631474d740c4c470c4f2cb17f3f2378e208a2d43
ScottLe11 <60492217+ScottLe11@users.noreply.github.com>
2020-10-26 16:25:22 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//Spell #1
+
+$(".Hide").click(function(){
+ $(".Hidepic").toggle();
+ $(".Hide").text("Hide/Show");
+ 
+});
+
+
+//Spell #2
+
+$(".fadeButton").click(function(){
+$(".fadePic").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$(".slideButton").click(function(){
+ $(".slidePic").slideToggle();
+});
+
+
+
